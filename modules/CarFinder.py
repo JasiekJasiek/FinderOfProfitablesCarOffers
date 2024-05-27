b42432acd61342661384f7a716b56f8efb89416f
mierzwu <mierzwickiszymon1@gmail.com>
2024-05-27 03:35:34 +0200

    CarFinder.py

--- modules/CarFinder.py
+++ modules/CarFinder.py
@@ -1,17 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 
-
 class CarFinder:
 
     def __init__(self):
-        self.response = requests.get("https://www.otomoto.pl/osobowe?search%5Border%5D=created_at_first%3Adesc")
+        self.response = requests.get('https://www.otomoto.pl/osobowe?search%5Border%5D=created_at_first%3Adesc')
 
+    def extract_url_from_html_tag (self, html_tag):
+        html_tag = html_tag.removeprefix('<a href="')
+        html_tag = html_tag.removesuffix('.html*')
+        return html_tag[0:html_tag.find('"')]
+    
     def look_for_new_car(self):
-        soup = BeautifulSoup(self.response.text, "html.parser")
-        offer1 = soup.find("h1")
-        offer1 = str(offer1.find("a"))
-        offer1 = offer1.removeprefix('<a href="')
-        offer1 = offer1.removesuffix('.html*')
-        url = offer1[0:url.find('"')]
-        return(url)
+        soup = BeautifulSoup(self.response.text, 'html.parser')
+        first_offer_url = soup.find('h1')
+        first_offer_url= str(first_offer_url.find('a'))
+        return self.extract_url_from_html_tag(first_offer_url)
