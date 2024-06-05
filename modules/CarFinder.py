3c70f93d538ba348a3eddb73418a2462da1b1a59
mierzwu <mierzwickiszymon1@gmail.com>
2024-06-06 00:23:39 +0200

    CarFinder.py

--- modules/CarFinder.py
+++ modules/CarFinder.py
@@ -1,17 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
+from modules.DatabaseHandler import DatabaseHandler
 
 class CarFinder:
 
     def __init__(self):
-        self.response = requests.get('https://www.otomoto.pl/osobowe?search%5Border%5D=created_at_first%3Adesc')
-
+        self.base_url = 'https://www.otomoto.pl/osobowe?search%5Border%5D=created_at_first%3Adesc'
+        
     def extract_url_from_html_tag (self, html_tag):
         html_tag = html_tag.removeprefix('<a href="')
         return html_tag[0:html_tag.find('"')]
     
     def look_for_new_car(self):
+        self.response = requests.get(self.base_url)
         soup = BeautifulSoup(self.response.text, 'html.parser')
         first_offer_url = soup.find('h1')
         first_offer_url= str(first_offer_url.find('a'))
-        return self.extract_url_from_html_tag(first_offer_url)
+        url = self.extract_url_from_html_tag(first_offer_url)
+        if DatabaseHandler.is_already_in_database(url):
+            return None
+        else:
+            return url
+        
