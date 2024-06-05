c69fe816e1d509cda2b6b736a853e83d6e4e7a5c
Jan Szala <94154383+JasiekJasiek@users.noreply.github.com>
2024-06-05 23:06:15 +0200

    Update Car.py

--- modules/Car.py
+++ modules/Car.py
@@ -1,12 +1,23 @@
 class Car:
 
-    def __init__(self, atributes) -> None:
-        print(atributes)
+    def __init__(self, atributes):
         self.accident_free = atributes[ 0 ]
         self.brand = atributes[ 1 ]
         self.course = atributes[ 2 ]
-        self.exploitation = atributes[ 3 ] 
+        self.exploitation = atributes[ 3 ]
         self.horse_power = atributes[ 4 ]
         self.model = atributes[ 5 ]
         self.price = atributes[ 6 ]
         self.production_year = atributes[ 7 ]
+
+    def to_list(self):
+        return [
+            self.accident_free,
+            self.brand,
+            self.course,
+            self.exploitation,
+            self.horse_power,
+            self.model,
+            self.price,
+            self.production_year
+        ]
