233efa27ab0dde300e6d14c4acf04988a2d9222b
Jan Szala <94154383+JasiekJasiek@users.noreply.github.com>
2024-06-08 01:18:21 +0200

    Update Functions.py

--- modules/Functions.py
+++ modules/Functions.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
+#get soup class varable from given site
 def get_soup(URL: str) -> BeautifulSoup:
     page = requests.get(URL)
+    #sometimes page is empty
+    if page.text == '':
+        return get_soup
     return BeautifulSoup(page.text, 'html.parser')
