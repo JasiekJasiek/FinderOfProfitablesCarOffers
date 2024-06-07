8d23edc08ebf5f49244ee828ada7bc687ec4d8a7
Jan Szala <94154383+JasiekJasiek@users.noreply.github.com>
2024-06-07 02:50:32 +0200

    Update CarFinder.py

--- modules/CarFinder.py
+++ modules/CarFinder.py
@@ -8,8 +8,9 @@ class CarFinder:
     def __init__(self):
         self.base_url = 'https://www.otomoto.pl/osobowe?search%5Border%5D=created_at_first%3Adesc'
         
-    def extract_url_from_html_tag (self, html_tag):
-        html_tag = html_tag.removeprefix('<a href="')
+    def extract_url_from_html_tag (self, html_tag: str):
+        i = html_tag.find('href="')
+        html_tag = html_tag[i + 6::]
         return html_tag[0:html_tag.find('"')]
     
     def look_for_new_car(self):
