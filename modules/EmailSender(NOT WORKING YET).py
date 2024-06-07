38ce3a5a99f6542443b815bc758aac0146a87cfe
mierzwu <mierzwickiszymon1@gmail.com>
2024-06-08 01:26:00 +0200

    Add files via upload

---
+++ modules/EmailSender(NOT WORKING YET).py
@@ -0,0 +1,25 @@
+import smtplib
+import ssl
+from email.mime.text import MIMEText
+
+
+class EmailSender:
+    
+    @staticmethod
+    def send_email(email) -> None:
+        sender = 'finderofprofitablescaroffers@gmail.com'
+        reciver = [str(email)]
+        s = smtplib.SMTP('smtp.gmail.com', 587)
+        s.connect()
+        s.starttls()
+        s.login(sender,'lxaf xyyi arwd blsv')
+        msg = MIMEText('I FOUND NEW INTRESTING OFFER JUST FOR U <3')
+        msg['subject'] = 'NEW CAR OFFER!'
+        msg['from'] = sender
+        msg['to'] = reciver
+        s.sendmail(sender,reciver,msg.as_string())
+        s.quit()
+        print("email sent")
+    
+    
+
