6e0a4932a5f05de2c85d0bd2002bfb5b7f76ce01
Jan Szala <janszala.04@gmail.com>
2024-06-08 20:01:21 +0200

    commit

--- modules/DatabaseHandler.py
+++ modules/DatabaseHandler.py
@@ -1,6 +1,7 @@
 import sqlite3
 from modules.Car import Car
 from modules.Scraper import Scraper
+import math
 
 class DatabaseHandler:
     
@@ -27,3 +28,35 @@ class DatabaseHandler:
         DatabaseHandler.database.execute("SELECT * FROM Cars")
         response = DatabaseHandler.database.fetchall()
         print(response)
+
+    @staticmethod
+    def get_prices_for_similar_car(car: Car) -> list[int]:
+        DatabaseHandler.database.execute(f"""
+                                        SELECT price 
+                                        FROM Cars 
+                                        WHERE brand = '{car.brand}' AND 
+                                              model = '{car.model}' AND 
+                                              production_year <= '{int(car.production_year) + 1}' AND 
+                                              production_year >= '{int(car.production_year) - 1}' AND
+                                              horse_power <= '{int(car.horse_power) + 20}' AND
+                                              horse_power >= '{int(car.horse_power) - 20}'
+                                        """)
+        response = DatabaseHandler.database.fetchall()
+        return [ int(line[ 0 ]) for line in response]
+    
+    @staticmethod
+    def get_avg_price_for_similar_car_through_years(car: Car) -> list[int]:
+        DatabaseHandler.database.execute(f"""
+                                        SELECT production_year, AVG(price) 
+                                        FROM Cars 
+                                        WHERE brand = '{car.brand}' AND 
+                                              model = '{car.model}' AND 
+                                              production_year <= '{int(car.production_year) + 1}' AND 
+                                              production_year >= '{int(car.production_year) - 1}' AND
+                                              horse_power <= '{int(car.horse_power) + 20}' AND
+                                              horse_power >= '{int(car.horse_power) - 20}'
+                                        GROUP BY production_year
+                                        """)
+        response = DatabaseHandler.database.fetchall()
+        return ([int(line[ 0 ]) for line in response], [int(line[ 1 ]) for line in response])
+    
---
+++ test.py
@@ -0,0 +1,15 @@
+from modules.DatabaseHandler import DatabaseHandler
+import statistics
+from modules.Car import Car
+from modules.Scraper import Scraper
+import matplotlib.pyplot as plt
+
+scraper = Scraper()
+prices = DatabaseHandler.get_prices_for_similar_car(scraper.scrap_car_atributes('https://www.otomoto.pl/osobowe/oferta/volkswagen-golf-golf-8-ID6G0bm1.html'))
+
+
+
+year, price = DatabaseHandler.get_avg_price_for_similar_car_through_years(scraper.scrap_car_atributes('https://www.otomoto.pl/osobowe/oferta/volkswagen-golf-golf-8-ID6G0bm1.html'))
+
+
+plt.show()
